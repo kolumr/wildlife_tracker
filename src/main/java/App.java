a1b3b31e96c46ffc1af5b4c7e4eaaacaa8c98023
Ronald Kolum <kolumronnie@gmail.com>
2022-03-15 12:18:29 +0300

    added hbs to retrieve all items

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,10 @@
 import models.dao.AnimalImplementationDAO;
 import models.dao.EndangeredAnimalImplementationDAO;
 import models.dao.SightingImplementationDAO;
+import models.pojos.Animals;
 import org.sql2o.Sql2o;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import com.google.gson.Gson;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -23,6 +25,12 @@ public class App {
              Map<String, Object> model = new HashMap<>();
              return new ModelAndView(model, "index.hbs");
          }, new HandlebarsTemplateEngine());
+        get("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+//            List<Animals> animals = animalImplementationDAO.getAll();
+            model.put("animals",animalImplementationDAO.getAll());
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
          get("/animal-form", (request, response) -> {
              Map<String, Object> model = new HashMap<>();
              return new ModelAndView(model, "animal-form.hbs");
@@ -31,5 +39,17 @@ public class App {
              Map<String, Object> model = new HashMap<>();
              return new ModelAndView(model, "endangered-animal-form.hbs");
          }, new HandlebarsTemplateEngine());
+        get("/sighting-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "endangered-animal-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        post ("/animal-success", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int id = Integer.parseInt(request.queryParams("id"));
+            String name = request.queryParams("name");
+            Animals animal = new Animals(id,name);
+            animalImplementationDAO.add(animal);
+            return new ModelAndView(model, "animal-form.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
