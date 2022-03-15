ce27f7adff0c363a3445dde10e0f2e5a540ae302
Ronald Kolum <kolumronnie@gmail.com>
2022-03-15 09:50:24 +0300

    implemented EndangeredAnimalIplementationDAO and SightingImplementationDAO methods

--- src/main/java/models/dao/EndangeredAnimalImplementationDAO.java
+++ src/main/java/models/dao/EndangeredAnimalImplementationDAO.java
@@ -1,7 +1,9 @@
 package models.dao;
 
 import models.pojos.EndangeredAnimals;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
@@ -12,12 +14,24 @@ public class EndangeredAnimalImplementationDAO implements EndangeredAnimalDAO{
     }
     @Override
     public void add(EndangeredAnimals endangeredAnimal) {
-
+        String sql = "INSERT INTO endangeredanimals (name,id,health,age) VALUES (:name,:id,:health,:age)";
+        try(Connection con = sql2o.open()){
+            int id  = (int) con.createQuery(sql,true)
+                    .bind(endangeredAnimal)
+                    .executeUpdate()
+                    .getKey();
+            endangeredAnimal.setId(id);
+        }catch (Sql2oException sql2oException){
+            sql2oException.printStackTrace();
+        }
     }
 
     @Override
     public List<EndangeredAnimals> getAll() {
-        return null;
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM endangeredanimals")
+                    .executeAndFetch(EndangeredAnimals.class);
+        }
     }
 
     @Override
@@ -27,11 +41,25 @@ public class EndangeredAnimalImplementationDAO implements EndangeredAnimalDAO{
 
     @Override
     public void deleteById(int id) {
-
+        String sql = "DELETE from endangeredanimals WHERE id=:id"; //raw sql
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
     }
 
     @Override
     public void clearAll() {
-
+        String sql = "DELETE from endangeredanimals"; //raw sql
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
     }
+
 }
